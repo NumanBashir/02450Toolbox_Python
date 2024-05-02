1c3d09740544fe660188c840b951a7b44d6abf16
NumanBashir <sportybashir@gmail.com>
2024-05-02 12:28:33 +0200

    comment

--- MyTestFolder/ExamFolder/F19/F19_Q2_var_explained.py
+++ MyTestFolder/ExamFolder/F19/F19_Q2_var_explained.py
@@ -1,7 +1,9 @@
 import numpy as np
 
 # Singular values from S matrix in the PCA decomposition
-singular_values = np.array([43.67, 33.47, 31.15, 30.36, 27.77, 13.86])
+singular_values = np.array([14.4, 8.19, 7.83, 6.91, 6.01])
+
+# TODO: If there is a statement about last 2 or similar, just cut the array to only have the ones asked about, ot just each of the explained variances
 
 # Total variance is the sum of squares of the singular values
 total_variance = np.sum(singular_values**2)
