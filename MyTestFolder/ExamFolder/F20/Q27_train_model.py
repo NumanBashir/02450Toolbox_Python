d6d6699a74fa3994df6ee0c5a816e8e1b734e8c7
NumanBashir <sportybashir@gmail.com>
2024-05-10 15:05:48 +0200

    Add newline at the end of the file

--- MyTestFolder/ExamFolder/F20/Q27_train_model.py
+++ MyTestFolder/ExamFolder/F20/Q27_train_model.py
@@ -9,3 +9,4 @@ final_optimal_lambda_training = 1
 total_uses = outer_folds_used * (loocv_trainings_per_fold * models_per_lambda) + final_optimal_lambda_training
 
 print(f"Total times each observation is used for training: {total_uses}")
+ 
