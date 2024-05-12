eed04a2e8fca83e99c4edec601594bf4f6022f35
NumanBashir <sportybashir@gmail.com>
2024-05-12 17:29:17 +0200

    Refactor K-means clustering code for better readability and maintainability

--- MyTestFolder/ExamFolder/F17/Q8_converged_table.py
+++ MyTestFolder/ExamFolder/F17/Q8_converged_table.py
@@ -7,22 +7,8 @@ heights = np.array([5.7, 6.0, 6.2, 6.3, 6.4, 6.6, 6.7, 6.9, 7.0, 7.4]).reshape(-
 player_labels = np.array([1, 1, 1, 1, 2, 2, 2, 1, 1, 2])  # For visualization purposes
 
 # K-means clustering
-kmeans = KMeans(n_clusters=2, random_state=0).fit(heights)
+kmeans = KMeans(n_clusters=2, random_state=0).fit(heights) # TODO MAKE SURE TO CHECK THE CLUSTER NUMBER
 clusters = kmeans.labels_
-
-# Visualization
-plt.figure(figsize=(10, 6))
-plt.scatter(heights, np.zeros_like(heights), c=clusters, s=100, cmap='viridis')
-plt.title('K-Means Clustering of Basketball Player Heights')
-plt.xlabel('Height')
-plt.yticks([])
-plt.grid(True)
-
-# Adding player labels for clarity
-for i, txt in enumerate(player_labels):
-    plt.annotate(f'O{txt}', (heights[i], 0))
-
-plt.show()
 
 # Print the cluster assignments
 print("Cluster Assignments:", clusters)
