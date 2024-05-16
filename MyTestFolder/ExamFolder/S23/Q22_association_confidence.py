7751f270f9d89fc6828fa029c177f7d498567355
alecranjitkar <alexranjitkar@gmail.com>
2024-05-16 15:49:33 +0200

    updates

--- MyTestFolder/ExamFolder/S23/Q22_association_confidence.py
+++ MyTestFolder/ExamFolder/S23/Q22_association_confidence.py
@@ -1,3 +1,4 @@
+from fractions import Fraction
 # Given values
 support_X = 3 / 5
 support_Y = 8 / 15
@@ -8,9 +9,11 @@ support_X_union_Y = confidence_X_to_Y * support_X
 
 # Calculate the confidence of Y → X
 confidence_Y_to_X = support_X_union_Y / support_Y
+confidence_fraction = Fraction(confidence_Y_to_X).limit_denominator() 
 
 print(f"Support of X: {support_X}")
 print(f"Support of Y: {support_Y}")
 print(f"Support of X ∪ Y: {support_X_union_Y}")
 print(f"Confidence of X → Y: {confidence_X_to_Y}")
 print(f"Confidence of Y → X: {confidence_Y_to_X}")
+print(f"Confidence of Y → X as a fraction: {confidence_fraction}")
